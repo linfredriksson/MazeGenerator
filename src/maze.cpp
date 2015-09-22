0aae8a0fe26493fb8056de12e0b8ce244a29976a
Linus Fredriksson <linfredriksson@gmail.com>
2015-09-22 12:23:54 +0200

    - Change maze class to namespace. - Add empty function for generating a maze using Prim's algorithm. - Add nameless namespace in the Maze namespace that will contain helper functions not visible from outside.

--- src/maze.cpp
+++ src/maze.cpp
@@ -1,1 +1,11 @@
 #include "maze.h"
+
+namespace Maze
+{
+	namespace
+	{
+		// helpter functions
+	}
+
+	void PrimsAlgorithm(unsigned char &maze);
+};
--- src/maze.h
+++ src/maze.h
@@ -1,6 +1,8 @@
 #pragma once
 
-class Maze
+namespace Maze
 {
-	
+	// Maze based on Prim's algorithm. For more information please follow the link
+	// https://en.wikipedia.org/wiki/Maze_generation_algorithm
+	void PrimsAlgorithm(int sizeX, int sizeY, unsigned char &maze);
 };
