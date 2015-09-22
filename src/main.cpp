d2c14c44f5003fd262ae9347e77dcbf79e34305a
Linus Fredriksson <linfredriksson@gmail.com>
2015-09-22 13:10:25 +0200

    - Add test code to print a maze to console.

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,24 @@
 
 int main()
 {
+	int width = 4;
+	int height = 3;
+	unsigned char maze[] = {
+		0, 0, 0, 0,
+		1, 0, 1, 0,
+		1, 0, 0, 0
+	};
+
+	for (int y = 0; y < height; ++y)
+	{
+		for (int x = 0; x < width; ++x)
+		{
+			maze[x + y * width] ? printf("1") : printf("0");
+		}
+		printf("\n");
+	}
+
+	int a; std::cin >> a; // pause program
+
 	return 0;
 }
